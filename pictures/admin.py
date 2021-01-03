6e2f1f43a69b09c92ef4fd27173e683fd38cbd2c
he-is-zzy <2892728102@qq.com>
2021-01-03 23:21:48 +0800

    test project

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import info, style
 
 # Register your models here.
+admin.site.register(style)
+admin.site.register(info)
--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,27 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+
+
+class style(models.Model):
+    style_name = models.CharField(max_length=200)
+    style_num = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.style_name
+
+
+class info(models.Model):
+    info_path = models.CharField(max_length=200)
+    info_size = models.CharField(max_length=20)
+    info_date = models.DateField('date published')
+
+    def recently(self):
+        return self.info_date >= timezone.now()-datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.info_path
